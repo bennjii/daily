90296daa0a85214c3aa11bd44619e4d9727ff61c
UnRealReincarlution <benwhite2413@gmail.com>
2021-04-19 17:33:13 +1200

    Solid Rendering - Permissive States

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import axios from 'axios'
 
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import invert from '../public/invert'
 
-import SettingsMenu from '../public/components/settings'
+import SettingsMenu from '@components/settings'
+import QuoteOfTheDay from '@components/qotd'
 
 import { Box, Check, Eye, Plus, Settings, Square, Trash, X } from 'react-feather'
-import { toUnicode } from 'node:punycode'
 
 export default function Home() {
   const [ date, setDate ] = useState(new Date());
@@ -19,23 +19,29 @@ export default function Home() {
   const color = "#ffffff"; //`#${invert(background?.color ? background.color : '#000000')}`;
   
   const [ todo, setTodo ] = useState((process.browser) && localStorage.getItem("todo") ? JSON.parse(localStorage.getItem("todo")) : [])
-  const [ documentSettings, setDocumentSettings ] = useState((process.browser) && localStorage.getItem("settings") ? JSON.parse(localStorage.getItem("settings")) : {
-	states: {
-	  editingTitle: false,
-	  settingsOpen: false,
-	},
-	settings: {
-	  title: 'things to do',
-	  showToDo: true,
-	  showAds: false,
-	  hour24: false,
-	  shortDate: false,
-	  quoteOfTheDay: false
-	}
-  });
+  const [ documentSettings, setDocumentSettings ] = useState(
+	(process.browser) && localStorage.getItem("settings") ? 
+		JSON.parse(localStorage.getItem("settings")) 
+	: 
+		{
+			states: {
+				editingTitle: false,
+				settingsOpen: false,
+				},
+			settings: {
+				title: 'things to do',
+				showToDo: true,
+				showAds: false,
+				hour24: false,
+				shortDate: false,
+				quoteOfTheDay: false
+			}
+		}
+  );
 
   useEffect(() => {
 	console.log("Component Started");
+	console.log(documentSettings)
 
 	const repeat = () => {
 	  setDate(new Date());
@@ -55,7 +61,7 @@ export default function Home() {
 
   useEffect(() => {
 	localStorage.setItem("settings", JSON.stringify(documentSettings));
-	console.log("Settings Updated")
+	console.log("Settings Updated");
   }, [documentSettings])
 
   return (
@@ -67,14 +73,17 @@ export default function Home() {
 
 		<div className={styles.leftSide}>
 			<div>
-				<div hidden={!documentSettings.settings.showAds}>
-					<h3 style={{ color:  color }}>AD</h3>
-				</div>
+				{
+					(documentSettings.settings.showAds) 
+					&&
+					<div>
+						<h3 style={{ color:  color }}>AD</h3>
+					</div>
+				}
 				
-				<div hidden={!documentSettings.settings.quoteOfTheDay}>
-					<h3 style={{ color:  color, fontWeight: 100 }}>{quoteOfTheDay?.contents?.quotes[0]?.quote}</h3>
-					<h4 style={{ color:  color, fontWeight: 400 }}>{quoteOfTheDay?.contents?.quotes[0]?.author}</h4>
-				</div>
+				{
+					(documentSettings.settings.quoteOfTheDay) && <QuoteOfTheDay qotd={quoteOfTheDay} color={color} />
+				}
 			</div>
 
 			<div className={styles.time}>
@@ -98,121 +107,126 @@ export default function Home() {
 				</div>
 			</div>
 		</div>
+		
+	  	<div className={styles.rightSide}>
+			{
+				(process.browser && documentSettings.settings.showToDo) ?
+				<div>
+					<div className={styles.todoHeader}>
+						{
+							documentSettings.settings.editingTitle ?
+							<input type="text" placeholder={documentSettings.settings.title} 
+							onChange={(e) => setDocumentSettings({...documentSettings, title: e.target.value })} 
+							onKeyDown={(e) => {
+								if(e.key == "Enter") setDocumentSettings({...documentSettings, states: { ...documentSettings.states, editingTitle: false } })
+							}} autoFocus/>
+							:
+							<h2 onClick={() => setDocumentSettings({...documentSettings, states: { ...documentSettings.states, editingTitle: true } })}>{documentSettings.settings.title}</h2>
+						}
+					
+						<Plus color={"#000000"} size={20} strokeWidth={1.5} onClick={() => {
+							todo.push({
+								editable: true,
+								title: '',
+								completed: false
+							})
 
-	  	<div className={styles.rightSide} hidden={!documentSettings.settings.showToDo}>
-			<div className={styles.toDo}>
-				<div className={styles.todoHeader}>
-					{
-						documentSettings.settings.editingTitle ?
-						<input type="text" placeholder={documentSettings.settings.title} 
-						onChange={(e) => setDocumentSettings({...documentSettings, title: e.target.value })} 
-						onKeyDown={(e) => {
-							if(e.key == "Enter") setDocumentSettings({...documentSettings, states: { ...documentSettings.states, editingTitle: false } })
-						}} autoFocus/>
-						:
-						<h2 onClick={() => setDocumentSettings({...documentSettings, states: { ...documentSettings.states, editingTitle: true } })}>{documentSettings.settings.title}</h2>
-					}
-				
-					<Plus color={"#000000"} size={20} strokeWidth={1.5} onClick={() => {
-						todo.push({
-							editable: true,
-							title: '',
-							completed: false
-						})
-
-						localStorage.setItem("todo", JSON.stringify(todo));
-					}}/>
-				</div>
-				<div className={styles.todoBody}>
-					{
-						todo.map((e, index) => {
-							return (
-								<div key={`TODO${index}`} onClick={(e) => {
-										//@ts-ignore
-										if(e.target.nodeName == "DIV") {
-										todo[index].completed = !todo[index].completed;
-										localStorage.setItem("todo", JSON.stringify(todo));
-										}
-									}}>
-
-									{
-									(e.editable)
-									?
-									<div>
-										<input type="text" defaultValue={e.title} placeholder={"Click to edit me"} onBlur={(e) => { 
-											todo[index] = {
-												editable: false,
-												title: e.target.value,
-												completed: false
-											}
-										}} onKeyDown={(e) => {
-											if(e.key == "Enter") {
-												todo[index] = {
-												editable: false,
-												//@ts-ignore
-												title: e.target.value,
-												completed: false
-												}
-
-												localStorage.setItem("todo", JSON.stringify(todo));
-											}
-										}} autoFocus/>
-									</div>
-									:
-									<div className={(e.completed) ? styles.completedTask : styles.uncompletedTask }>
-										<div className={styles.todoLabel}>
-											<p onClick={() => {
-												todo[index].editable = true
-											}}>{e.title}</p>
-										</div>
-
-										<div>
-										{
-											(e.completed)
-											?
-											<Check color={(e.completed) ? "#226d38" : "#3b3b3b"} size={20}  onClick={(e) => {
-												todo[index].completed = false;
-												localStorage.setItem("todo", JSON.stringify(todo));
-											}}/>
-											:
-											<Square color={(e.completed) ? "#226d38" : "#3b3b3b"} size={20} onClick={(e) => {
-												todo[index].completed = true;
-												localStorage.setItem("todo", JSON.stringify(todo));
-											}}/>
-										}
-										</div>
-
-										<Trash color={(e.completed) ? "#226d38" : "#3b3b3b"} size={20} onClick={(e) => {
-											todo.splice(index, 1);
+							localStorage.setItem("todo", JSON.stringify(todo));
+						}}/>
+					</div>
+					<div className={styles.todoBody}>
+						{
+							todo.map((e, index) => {
+								return (
+									<div key={`TODO${index}`} onClick={(e) => {
+											//@ts-ignore
+											if(e.target.nodeName == "DIV") {
+											todo[index].completed = !todo[index].completed;
 											localStorage.setItem("todo", JSON.stringify(todo));
-											}} onMouseOver={(e) => {
-											//@ts-expect-error
-											if(e.target.nodeName == 'path' || e.target.nodeName == 'polyline') {
-												//@ts-expect-error
-												e.target.parentElement.classList.add(styles.todoTrashHover)
-											}else {
-												//@ts-expect-error
-												e.target.classList.add(styles.todoTrashHover)
 											}
-										
-										}} onMouseLeave={(e) => {
-											//@ts-expect-error
-											if(e.target.nodeName == 'path' || e.target.nodeName == 'polyline') {
-												//@ts-expect-error
-												e.target.parentElement.classList.remove(styles.todoTrashHover)
-											}else {
-												//@ts-expect-error
-												e.target.classList.remove(styles.todoTrashHover)
+										}}>
+
+										{
+										(e.editable)
+										?
+										<div>
+											<input type="text" defaultValue={e.title} placeholder={"Click to edit me"} onBlur={(e) => { 
+												todo[index] = {
+													editable: false,
+													title: e.target.value,
+													completed: false
+												}
+											}} onKeyDown={(e) => {
+												if(e.key == "Enter") {
+													todo[index] = {
+													editable: false,
+													//@ts-ignore
+													title: e.target.value,
+													completed: false
+													}
+
+													localStorage.setItem("todo", JSON.stringify(todo));
+												}
+											}} autoFocus/>
+										</div>
+										:
+										<div className={(e.completed) ? styles.completedTask : styles.uncompletedTask }>
+											<div className={styles.todoLabel}>
+												<p onClick={() => {
+													todo[index].editable = true
+												}}>{e.title}</p>
+											</div>
+
+											<div>
+											{
+												(e.completed)
+												?
+												<Check color={(e.completed) ? "#226d38" : "#3b3b3b"} size={20}  onClick={(e) => {
+													todo[index].completed = false;
+													localStorage.setItem("todo", JSON.stringify(todo));
+												}}/>
+												:
+												<Square color={(e.completed) ? "#226d38" : "#3b3b3b"} size={20} onClick={(e) => {
+													todo[index].completed = true;
+													localStorage.setItem("todo", JSON.stringify(todo));
+												}}/>
 											}
-										}}/>
-									</div> 
-									}
-								</div>
-							)
-						})
-					}
-				</div>
-			</div> 
+											</div>
+
+											<Trash color={(e.completed) ? "#226d38" : "#3b3b3b"} size={20} onClick={(e) => {
+												todo.splice(index, 1);
+												localStorage.setItem("todo", JSON.stringify(todo));
+												}} onMouseOver={(e) => {
+												//@ts-expect-error
+												if(e.target.nodeName == 'path' || e.target.nodeName == 'polyline') {
+													//@ts-expect-error
+													e.target.parentElement.classList.add(styles.todoTrashHover)
+												}else {
+													//@ts-expect-error
+													e.target.classList.add(styles.todoTrashHover)
+												}
+											
+											}} onMouseLeave={(e) => {
+												//@ts-expect-error
+												if(e.target.nodeName == 'path' || e.target.nodeName == 'polyline') {
+													//@ts-expect-error
+													e.target.parentElement.classList.remove(styles.todoTrashHover)
+												}else {
+													//@ts-expect-error
+													e.target.classList.remove(styles.todoTrashHover)
+												}
+											}}/>
+										</div> 
+										}
+									</div>
+								)
+							})
+						}
+					</div>
+				</div> 
+				:
+				<></>
+			}
 	  	</div>
 	  
 		{
